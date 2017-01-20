ac6be0dd44dacd82110b9f996480fd1e4464b12f
wodeccu <wodecc_u@sina.com>
2017-01-20 16:18:54 +0800

    +完成文章添加功能

--- Apps/Admin/Controller/ArticleController.class.php
+++ Apps/Admin/Controller/ArticleController.class.php
@@ -13,12 +13,25 @@ class ArticleController extends Controller {
     	$this->assign('list',$list);
     	$this->display();
     }
-    public function art_list(){
-    	$this->display();
-    }
     public function art_add(){
     	$this->display();
     }
+    public function do_add(){
+    	$json = file_get_contents ( "php://input" ); //获取前台ajax过来的json
+    	$json = json_decode ( $json );//将json字符串转成对象，如果带第二个参数true，则转成数组
+    	$m = M('article');
+    	$data['title'] = $json->title;
+    	$data['cid'] = $json->column;
+    	$data['create_time'] = date('y-m-d H:i:s', time());
+    	$data['context'] = $json->context;
+    	$result = $m->add($data);
+    	if($result !== false){
+    		echo 1;
+    	}else{
+    		echo 0;
+    	}
+    }
+    
     public function art_edit(){
     	$this->display();
     }
