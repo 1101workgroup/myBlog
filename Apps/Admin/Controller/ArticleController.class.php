a1d1f38528edd23acac56578b33b0ed8ce4fdb25
wodeccu <wodecc_u@sina.com>
2017-01-20 19:59:23 +0800

    +完成文章删除、文章编辑功能

--- Apps/Admin/Controller/ArticleController.class.php
+++ Apps/Admin/Controller/ArticleController.class.php
@@ -36,9 +36,40 @@ class ArticleController extends Controller {
     }
     
     public function art_edit(){
+    	$col = M('mb_column');
+    	$colresult = $col->select();
+    	$this->assign('list',$colresult);
+    	$id = I('get.id','');
+    	$con['id'] = $id;
+    	$m = M('mb_article');
+    	$result = $m->where($con)->find();
+    	$this->assign('result',$result);
     	$this->display();
     }
+    public function do_edit(){
+    	$json = file_get_contents ( "php://input" ); //获取前台ajax过来的json
+    	$json = json_decode ( $json );//将json字符串转成对象，如果带第二个参数true，则转成数组
+    	$con['id'] =  $json->id;
+    	$data['title'] = $json->title;
+    	$data['cid'] = $json->column;
+    	$data['context'] = $json->context;
+    	$m = M('mb_article');
+    	$result = $m->where($con)->save($data);
+    	if($result !== false){
+    		echo 1;
+    	}else{
+    		echo 0;
+    	}
+    }
     public function art_delete(){
-    	$this->display();
+    	$id = I('post.id','');
+    	$con['id'] = $id;
+    	$m = M('mb_article');
+    	$result = $m->where($con)->delete();
+    	if($result){
+    		echo 1;
+    	}else{
+    		echo 0;
+    	}
     }
 }
