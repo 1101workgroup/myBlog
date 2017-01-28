42190eac19b6bfce796e2f012697f92584cacea7
wodeccu <wodecc_u@sina.com>
2017-01-28 16:35:12 +0800

    +用户注册验证码判断，用户名唯一性判断

--- Apps/Home/Controller/LoginController.class.php
+++ Apps/Home/Controller/LoginController.class.php
@@ -22,22 +22,38 @@ class LoginController extends Controller {
     	$this->display();
     }
     public function Verification(){
-    	$image = imagecreatetruecolor(100, 30);
+    	$image = imagecreatetruecolor(100, 30); 
     	$bgcolor = imagecolorallocate($image, 255, 255, 255);
     	imagefill($image, 0, 0, $bgcolor);
     	 
+//     	for($i=0;$i<4;$i++){
+//     		$fontsize=6;
+//     		$fontcolor = imagecolorallocate($image, rand(0,120), rand(0,120), rand(0,120));
+//     		$fontcontent = rand(0,9);
+//     		$x = ($i*100/4)+rand(5,10);
+//     		$y = rand(5,10);
+//     		imagestring($image, $fontsize, $x, $y, $fontcontent, $fontcolor);
+//     	}
+		$captch_code = '';
     	for($i=0;$i<4;$i++){
     		$fontsize=6;
     		$fontcolor = imagecolorallocate($image, rand(0,120), rand(0,120), rand(0,120));
-    		$fontcontent = rand(0,9);
+    		$data = 'abcdefghijkmnpkrstuvwxyz23456789';
+    		$fontcontent = substr($data, rand(0,strlen($data)),1);
+    		$captch_code .= $fontcontent;
     		$x = ($i*100/4)+rand(5,10);
     		$y = rand(5,10);
     		imagestring($image, $fontsize, $x, $y, $fontcontent, $fontcolor);
     	}
-    	 
+    	$_SESSION['authcode'] = $captch_code;
     	for($i=0;$i<200;$i++){
     		$pointcolor = imagecolorallocate($image, rand(50,200), rand(50,200), rand(50,200));
     		imagesetpixel($image, rand(1,99), rand(1,29), $pointcolor);
+    	}
+    	
+    	for ($i=0;$i<3;$i++){
+    		$linecolor = imagecolorallocate($image, rand(80,200), rand(80,220), rand(80,200));
+    		imageline($image, rand(1,99), rand(1,29), rand(1,99), rand(1,29), $linecolor);
     	}
     	 
     	header("Content-type: image/png");
@@ -49,12 +65,22 @@ class LoginController extends Controller {
     	$json = json_decode($json);
     	$data['user'] = $json->user;
     	$data['pwd'] = $json->pwd;
-		$m = M('user');
-		$result = $m->add($data);
-		if($result){
-			$this->ajaxReturn(array('code'=>1,'message'=>'注册成功'));
-		}else{
-			$this->assign(array('code'=>0,'message'=>'注册失败'));
-		}
+    	$authcode = strtolower($json->authcode);
+    	if($authcode == $_SESSION['authcode']){
+    		$m = D('user');
+    		if($m->create($data)){
+    			if($m->add()){
+    				$this->ajaxReturn(array('code'=>1,'message'=>'注册成功'));
+    			}else{
+    				$this->ajaxReturn(array('code'=>0,'message'=>'注册失败'));
+    			}
+    		}else{
+    			$this->ajaxReturn(array('code'=>2,'message'=>$m->getError())) ;
+    		}
+    	}else{
+    		$this->ajaxReturn(array('code'=>2,'message'=>'验证码错误'));
+    	
+    	}
+		
     }
 }
