f9ce0259a5c5edeaf305b2fe5775fca9e84ae769
wodeccu <wodecc_u@sina.com>
2017-02-03 23:45:48 +0800

    +最新发表限制10条，文章列表按时间 desc排序

--- Apps/Home/Controller/IndexController.class.php
+++ Apps/Home/Controller/IndexController.class.php
@@ -6,9 +6,9 @@ class IndexController extends Controller {
     public function index(){
     	$cateList = M('cate')->select();
     	$this->assign('cateList',$cateList);
-    	$artList = M('article')->select();
+    	$artList = M('article')->order('create_time desc')->select();
     	$this->assign('artList',$artList);
-    	$newList = M('article')->order('create_time desc')->select();
+    	$newList = M('article')->order('create_time desc')->limit(0,10)->select();
     	$this->assign('newList',$newList);
     	$this->display();
     }
