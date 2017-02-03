2bface50275c49dcead6cb57e78b8f56d8157d55
wodeccu <wodecc_u@sina.com>
2017-02-03 17:46:46 +0800

    +用户登录首页 导航条

--- Apps/Home/Controller/IndexController.class.php
+++ Apps/Home/Controller/IndexController.class.php
@@ -4,8 +4,8 @@ use Think\Controller;
 class IndexController extends Controller {
 	//用户访问首页
     public function index(){
-    	$d=D('Index');
-    	$d->echo1();
+    	$cateList = M('cate')->select();
+    	$this->assign('cateList',$cateList);
     	$this->display();
     }
 }
