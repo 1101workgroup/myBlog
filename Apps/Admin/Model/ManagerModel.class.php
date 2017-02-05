d56a3e957a6a1d22793a5bc05161a600518e158b
wodeccu <wodecc_u@sina.com>
2017-02-05 11:28:41 +0800

    +管理员登录增加验证码，采用tp的验证码

--- Apps/Admin/Model/ManagerModel.class.php
+++ Apps/Admin/Model/ManagerModel.class.php
@@ -5,8 +5,13 @@ class ManagerModel extends Model{
 	protected $_validate = array(
 		array('user','require','用户名不能为空！',1,'regex',4),
 		array('pwd','require','密码不能为空',1,'regex',4),
+		array('verify','check_verify','验证码错误',1,'callback',4),
 			
 	);
+	public function check_verify($code, $id = ''){
+		$verify = new \Think\Verify();
+		return $verify->check($code, $id);
+	}
 	public function login(){
 		$pwd = $this->pwd;
 		$res = $this->where(array('user'=>$this->user))->find();
