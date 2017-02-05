8d8d95de5cd71d53e0201c7fee49fbb53fb01c61
wodeccu <wodecc_u@sina.com>
2017-02-05 11:09:11 +0800

    +管理员登录改成使用model自动验证

--- Apps/Admin/Controller/LoginController.class.php
+++ Apps/Admin/Controller/LoginController.class.php
@@ -14,16 +14,26 @@ class LoginController extends Controller {
 		$json = file_get_contents ( "php://input" ); //获取前台ajax过来的json
  		$json = json_decode ( $json );//将json字符串转成对象，如果带第二个参数true，则转成数组 
 		$con ['user'] = $json->user;
-		$con ['pwd'] = md5 ( $json->pwd );
-		$m = M ( 'manager' );
-		$result = $m->where ( $con )->find ();
-		if ($result != null) {
-			$_SESSION['manager'] = $result['id'];
-			$_SESSION['manager-user'] = $result['name'];
-			echo 1;
-		} else {
-			echo 0;
+		$con ['pwd'] = $json->pwd;
+		$m = D ( 'manager' );
+		if($m->create($con,4)){
+			if($m->login()){
+				$this->ajaxReturn(array('code'=>'1'));
+			}else{
+				$this->ajaxReturn(array('code'=>'0','message'=>'用户名或密码错误'));
+			}
+		}else{
+			$this->ajaxReturn(array('code'=>'0','message'=>$m->getError()));
+			
 		}
+// 		$result = $m->where ( $con )->find ();
+// 		if ($result != null) {
+// 			$_SESSION['manager'] = $result['id'];
+// 			$_SESSION['manager-user'] = $result['name'];
+// 			echo 1;
+// 		} else {
+// 			echo 0;
+// 		}
 	}
 	public function logout(){
 		session_destroy();
---
+++ Apps/Admin/Model/ManagerModel.class.php
@@ -0,0 +1,21 @@
+<?php
+namespace Admin\Model;
+use Think\Model;
+class ManagerModel extends Model{
+	protected $_validate = array(
+		array('user','require','用户名不能为空！',1,'regex',4),
+		array('pwd','require','密码不能为空',1,'regex',4),
+			
+	);
+	public function login(){
+		$pwd = $this->pwd;
+		$res = $this->where(array('user'=>$this->user))->find();
+		if($res['pwd'] == md5($pwd)){
+			$_SESSION['manager'] = $res['id'];
+			$_SESSION['manager-user'] = $res['name'];
+			return true;
+		}else{
+			return false;
+		}
+	}
+}
